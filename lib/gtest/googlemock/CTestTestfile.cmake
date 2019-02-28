08ec971ba8abac65e623b8b3a23255a666b9e003
yngtodd <young.todd.mk@gmail.com>
2019-02-28 17:34:43 -0500

    update

---
+++ lib/gtest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/ygx/src/develop/torchkata/lib/gtest/googlemock
+# Build directory: /home/ygx/src/develop/torchkata/lib/gtest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ lib/gtest/googlemock/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ygx/src/develop/torchkata/lib/gtest/googletest
+# Build directory: /home/ygx/src/develop/torchkata/lib/gtest/googlemock/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
